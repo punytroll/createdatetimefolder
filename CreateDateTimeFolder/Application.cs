609694ff79bef6e29a584b216ee116c5b52d1dfc
Hagen M?bius <hagen.moebius@googlemail.com>
2013-05-14 13:23:20 +0200

    Initial import.

---
+++ CreateDateTimeFolder/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace CreateDateTimeFolder
+{
+    internal class Application
+    {
+        public static void Main(String[] Arguments)
+        {
+            Directory.CreateDirectory(Path.Combine(Arguments[0], DateTime.Now.ToString("yyyy-MM-dd HH-mm", CultureInfo.InvariantCulture)));
+        }
+    }
+}
